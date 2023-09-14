8f05874741276761eb065ee40a896203286793f0
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-09-13 22:53:25 -0700

    add more functions to index

--- index.js
+++ index.js
@@ -38,3 +38,70 @@ function viewEmployees() {
     startPrompt();
   });
 }
+
+function viewRoles() {
+  connection.query('SELECT * FROM role', function (error, data) {
+    console.table(data);
+    startPrompt();
+  });
+}
+
+function viewDepartments() {
+  connection.query('SELECT * FROM Department', function (error, data) {
+    console.table(data);
+    startPrompt();
+  });
+}
+
+function addEmployee() {
+  connection.query('SELECT * FROM role', function (error, roles) {
+    inquier
+      .prompt([
+        {
+          type: 'input',
+          name: 'first_name',
+          message: "what is the employee's first name?",
+        },
+        {
+          type: 'input',
+          name: 'last_name',
+          message: "what is the employee's last name?",
+        },
+        {
+          type: 'list',
+          name: 'role_id',
+          choices: roles.map((role) => role.title),
+          message: "what is the employee's role?",
+          filter: function (choice) {
+            return roles.find((role) => role.title === choice).id;
+          },
+        },
+        {
+          type: 'input',
+          name: 'manager_id',
+          message: "who is the employee's manager? (enter manager ID or NONE)",
+          filter: function (choice) {
+            if (choice.toUpperCase() === 'NONE') {
+              return null;
+            } else return choice;
+          },
+        },
+      ])
+      .then(function (res) {
+        const employee = {
+          manager_id: res.manager_id,
+          first_name: res.first_name,
+          last_name: res.last_name,
+          role_id: res.role_id,
+        };
+        connection.query(
+          'INSERT into employee SET ?',
+          employee,
+          function (error, data) {
+            console.table(data);
+            startPrompt();
+          }
+        );
+      });
+  });
+}
